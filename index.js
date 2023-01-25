64ddee175f333344379f279410faf089c94190d5
Yummo* <soroslinuor@gmail.com>
2023-01-25 16:28:53 +0200

    fix

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use("/api/uploads", express.static("uploads"));
 app.use("/api", router);
 app.use(errorMiddleware);
