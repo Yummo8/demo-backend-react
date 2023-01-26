747b43e9c33b17bff1d5114cf4879704e9328472
Yummo* <soroslinuor@gmail.com>
2023-01-26 19:31:35 +0200

    fix

--- service/token.service.js
+++ service/token.service.js
@@ -3,7 +3,7 @@ const tokenModel = require("../models/token.model");
 
 class TokenService {
   generateToken(payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: "1h" });
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: "15m" });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: "30d" });
     return {
       accessToken,
